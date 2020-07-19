8f9e335b0d38cd00596c3b51fb5c7cb0efa37319
bychen009 <bychen009@gmail.com>
2020-07-19 11:07:12 +0800

    Update autosignin.py

--- autosignin.py
+++ autosignin.py
@@ -1,12 +1,15 @@
 import requests
 
+username = os.environ["HAO4K_USERNAME"]
+password = os.environ["HAO4K_PASSWORD"]
+
 loginurl = 'https://www.hao4k.cn/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=LT3b2&inajax=1'
 signinurl = 'https://www.hao4k.cn/plugin.php?id=k_misign:sign&operation=qiandao&formhash=25c49317&format=empty'
 form_data = {
     'formhash': "03909abf",
     'referer': "https://www.hao4k.cn/",
-    'username': "bychen009@gmail.com",
-    'password': "cby890818",
+    'username': username,
+    'password': password,
     'questionid': "0",
     'answer': ""
 }
